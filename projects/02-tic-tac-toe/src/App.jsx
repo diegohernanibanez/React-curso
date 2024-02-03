691b56e55e7a58189e59fc82d7bf7137294c90e7
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2024-02-03 12:26:12 -0300

    Separando el codigo en componentes

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,36 +1,9 @@
 import { useState } from "react"
-
-const TURNS = {
-	X: 'x',
-	O: 'o'
-}
-
-const WINNER_COMBOS = [
-	[0,1,2],
-	[3,4,5],
-	[6,7,8],
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],
-	[0,4,8],
-	[2,4,6],
-]
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-
-	const className = `square ${isSelected ? 'is-selected' : ''}`
-
-	const handleClick = () => {
-		updateBoard(index)
-	}
-
-	return (
-		
-		<div onClick={handleClick} className={className}>
-			{children}
-		</div>
-	)
-}
+import { Square } from "./components/Square"
+import { WinnerModal } from "./components/WinnerModal"
+import { checkWinnerFrom, checkEndGame } from "./logic/board"
+import { TURNS } from "./constants"
+import confetti from "canvas-confetti"
 
 function App() {
 	const [board, setBoard] = useState(Array(9).fill(null))
@@ -39,29 +12,12 @@ function App() {
 
 	const [winner, setWinner] = useState(null) //null = sin gandor y false = empate
 
-	const checkWinner = (boardToCheck) => {
-		for(const combo of WINNER_COMBOS){
-			const [a,b,c] = combo
-			if(
-				boardToCheck[a] &&
-				boardToCheck[a] === boardToCheck[b] &&
-				boardToCheck[a] === boardToCheck[c]
-			){
-				return boardToCheck[a]
-			}
-		}
-	}
-
-	const checkEndGame = (boardToCheck) => {
-		return boardToCheck.every((square) => square  !== null)
-	}
-
 	const updateBoard = (index) => {
 		// no actualizamos esta posición si ya tiene algo
 		if(board[index] || winner){
 			return
 		}
-
+		
 		// actualizar el tablero
 		const newBoard = [... board]
 		newBoard[index] = turn
@@ -71,8 +27,9 @@ function App() {
 		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
 		setTurn(newTurn)
 		
-		const newWinner = checkWinner(newBoard)
+		const newWinner = checkWinnerFrom(newBoard)
 		if(newWinner){
+			confetti()
 			setWinner(newWinner)
 			// setWinner((prevWinner) => { // El prevWinner sirve para acceder al estado anterior
 			// 	return newWinner
@@ -111,28 +68,8 @@ function App() {
 				<Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
 				<Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
 			</section>
-			{
-				winner !== null && (
-					<section className="winner">
-						<div className="text">
-							<h2>
-								{
-									winner === false ? 'Empate' : 'Ganó:'
-								}
-							</h2>
 
-							<header className="win">
-								{
-									winner && <Square>{winner}</Square>
-								}
-							</header>
-							<footer>
-								<button onClick={resetGame}>Empezar de nuevo</button>
-							</footer>
-						</div>
-					</section>
-				)
-			}
+			<WinnerModal resetGame={resetGame} winner={winner}></WinnerModal>
 		</main>
 	)
 }
