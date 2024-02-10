9f5130edcd4780616114e8d0e44274e68637d4b0
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2024-02-10 16:26:39 -0300

    Agregando gitignore y Standard linter

--- projects/02-tic-tac-toe/src/components/Square.jsx
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types'
+
 export const Square = ({ children, isSelected, updateBoard, index }) => {
 
+	Square.propTypes = {
+		children: PropTypes.string,
+		isSelected: PropTypes.bool,
+		updateBoard: PropTypes.func,
+		index: PropTypes.number
+	}
+	
 	const className = `square ${isSelected ? 'is-selected' : ''}`
-
+	
 	const handleClick = () => {
 		updateBoard(index)
 	}
